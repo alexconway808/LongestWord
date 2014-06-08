50e448046f7e29c8c29d52e31e339fe0046761fa
Alex Conway <alexconway808@gmail.com>
2014-06-07 15:09:34 -1000

    connected the button and put the string and string array VARs after the button onclick

--- LW.js
+++ LW.js
@@ -2,27 +2,14 @@
 
 	var user_input = document.getElementById("user_input");
 	var button = document.getElementById("find");
-	var string = user_input.value;	
-	var string_array = string.split(" ");
 
 	button.onclick = function (){
+		var string = user_input.value;	
+		var string_array = string.split(" ");
 		string_array.sort(function(a, b) {
-	    return b.length - a.length;
+	    	return b.length - a.length;
 		});
+		alert(string_array[0]);
 	};
-	
-	alert(string_array[0]);
-	
-
-	// var user_input = document.getElementById("user_input");
-	// var button = document.getElementById("find");
-	// var string = "What is the longest"
-	// var string_array = string.split(" ")
-
-	// string_array.sort(function(a, b) {
-	//     return b.length - a.length;
-	// });
-
-	// alert(string_array[0]);
 
 })();
